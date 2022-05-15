bd560972c5a4a3a1e360c1a6d3427893e2506b7b
LeshaPI <pialeksej5@gmail.com>
2022-05-15 12:15:02 +0300

    feat: add mobile version(San,May 15,2022,12:14:48 PM)

--- cssMemSlider/Js/script.js
+++ cssMemSlider/Js/script.js
@@ -4,24 +4,40 @@ const sliderBody = document.querySelector('.slider');
 const sliderTrack = document.querySelector('.slider_track');
 const sliderItem = document.querySelectorAll('.slider_item');
 const bottomItem = document.querySelectorAll('.button_item');
-const textBody = document.querySelector('.slider_text');
+const textBody = document.querySelector('.slider_content');
 const visibletext = document.querySelector('.text_body');
 const text = document.querySelectorAll('.text');
 
-const itemWidth = sliderBody.clientWidth / visibleSlide;
-const textWidth = textBody.clientWidth / visibleSlide;
+let itemWidth = sliderBody.clientWidth / visibleSlide;
+let textWidth = textBody.clientWidth / visibleSlide;
 
-sliderItem.forEach((item) => {
-    item.style.minWidth = `${itemWidth}px`;
-})
+function ItemWithGeneration(index){
+    sliderTrack.style.left = `-${itemWidth * index}px`;
+    visibletext.style.left = `-${textWidth * index}px`;
+}
 
-text.forEach((item) =>{
-    item.style.minWidth = `${textWidth}px`;
-})
+function GenerateSlider(){
+    itemWidth = sliderBody.clientWidth / visibleSlide;
+    textWidth = textBody.clientWidth / visibleSlide;
+
+    sliderItem.forEach((item) => {
+        item.style.minWidth = `${itemWidth}px`;
+    })
+    
+    text.forEach((item) =>{
+        item.style.minWidth = `${textWidth}px`;
+    })
+}
+
+GenerateSlider(sliderItem,text)
+
+
+let lastIndex = 0;
+
 
 bottomItem.forEach((it,index) => {
     bottomItem[index].addEventListener('click',() =>{
-
+       lastIndex = index; 
        bottomItem[index].classList.add('animation'); 
        setTimeout(() => {bottomItem[index].classList.remove('animation')},300);
 
@@ -30,9 +46,26 @@ bottomItem.forEach((it,index) => {
        })
 
        bottomItem[index].classList.add('active');
-       sliderTrack.style.left = `-${itemWidth * index}px`;
-       visibletext.style.left = `-${textWidth*index}px`;
+       ItemWithGeneration(lastIndex);
     })
 })
 
 
+
+window.addEventListener('resize',() => {
+
+    if(window.matchMedia("(min-width: 768px)").matches) {
+        sliderBody.style.width = '600px';
+        textBody.style.width = '600px';
+        GenerateSlider();
+        ItemWithGeneration(lastIndex);
+    }
+    else if(window.matchMedia("(max-width: 768px)")){
+        sliderBody.style.width = '400px';
+        textBody.style.width = '400px';
+        GenerateSlider();
+        ItemWithGeneration(lastIndex);
+    }
+})
+
+  
